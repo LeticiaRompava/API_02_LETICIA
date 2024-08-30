cb086d7d08dd9c4665feaf3af470a320f08183dc
LeticiaRompava <leticia.rompava@escola.pr.gov.br>
2024-08-30 11:22:26 -0300

    subir

--- api.js
+++ api.js
@@ -35,6 +35,14 @@ const connection = mysql.createConnection(mysql_config)
 //6 cors
 app.use(cors());
 
+//tratamento dos posts params
+app.use(json());
+//instrução que pede que o express trate os dados como json
+
+app.use(express.urlencoded({extended: true}));
+//quando é enviado um pedido através do método post, os dados enviados
+//através 
+
 //7 rotas
 //rota inicial que vai dizer que a API está disponível 
 app.get('/', (req,res)=>{
@@ -64,7 +72,7 @@ app.get('/tasks/:id', (req,res)=>{
     })
 })
 
-//11 atualizar o status de uma task - método put
+//11 rota para atualizar o status de uma task - método put
 app.put('/tasks/:id/status/:status',(req,res)=>{
     const id = req.params.id;
     const status = req.params.status;
@@ -83,8 +91,51 @@ app.put('/tasks/:id/status/:status',(req,res)=>{
     })
 })
 
+//rota para deletar uma tarefa
+app.delete('/tasks/:id/delete',(req,res)=>{
+    const id = req.params.id;
+    connection.query('DELETE FROM tasks WHERE id=?',[id],(err,rows)=>{
+        if(!err){
+            if(rows.affectedRows){
+                res.json(functions.response('Sucesso','Task deletada',rows.affectedRows,null))
+            }
+            else{
+                res.json(functions.response('Atenção','Task não encontrada',0,null))
+            }
+        }
+        else{
+            res.json(functions.response('Erro',err.message,0,null))
+        }
+    })
+})
+
+//rota para inserir uma nova task
+app.put('/tasks/create',(req,res)=>{
+    //midleware para a recepção dos dados da tarefa(task)
+
+    //pegando os dados da request
+    const post_data = req.body;
+
+    //checar para ver se não estamos recebendo json vazia
+    if(post_data == undefined){
+        res.json(functions.response('Atenção','Sem dados de uma nova task',0,null))
+        return;
+    }
+    const task = post_data.task;
+    const status = post_data.status;
+
+    //inserindo a nova task
+    connection.query('INSERT INTO tasks(task,status,created_at,update_at) VALUES(?,?,NOW(),NOW()',[task,status],(err,rows)=>{
+        if(!err){
+            res.json('Sucesso','Task cadastrada no banco',rows.affectedRows,null)
+        }
+        else{
+            res.json(functions.response('Erro',err.message,0,null))
+        }
+    })
+})
+
 //8 midleware para caso alguma rota não seja encontrada
 app.use((req,res)=>{
     res.json(functions.response('atenção', 'Rota não encontrada',0,null))
 })
-
